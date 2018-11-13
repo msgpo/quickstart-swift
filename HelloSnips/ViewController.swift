6ac8e56d94ce0ea3bc84ad388d87d2422ca794d0
Michael Fester <michael.fester@gmail.com>
2018-11-13 13:50:21 +0100

    Initial commit

---
+++ HelloSnips/ViewController.swift
@@ -0,0 +1,104 @@
+//
+//  ViewController.swift
+//  HelloSnips
+//
+//  Copyright © 2018 Snips. All rights reserved.
+//
+
+import UIKit
+import SnipsPlatform
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    fileprivate let snips: SnipsPlatform
+    fileprivate let audioEngine: AVAudioEngine
+    fileprivate lazy var logView = UITextView()
+
+    init() {
+        let url = Bundle.main.url(forResource: "assistant", withExtension: nil)!
+        snips = try! SnipsPlatform(assistantURL: url)
+        audioEngine = try! ViewController.createAudioEngine(with: snips)
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Say the wake word"
+        
+        logView.font = UIFont.systemFont(ofSize: 16)
+        logView.frame = view.frame
+        logView.isUserInteractionEnabled = false
+        view.addSubview(logView)
+        
+        setupHandlers()
+        
+        do {
+            try snips.start()
+            try audioEngine.start()
+        } catch let e as SnipsPlatformError {
+            print("Snips error: \(e)")
+        } catch {
+            print("Unexpected error: \(error)")
+        }
+    }
+    
+    fileprivate class func createAudioEngine(with snips: SnipsPlatform) throws -> AVAudioEngine {
+        let audioEngine = AVAudioEngine()
+        let audioSession = AVAudioSession.sharedInstance()
+        try audioSession.setCategory(.playAndRecord, mode: .measurement, options: [.mixWithOthers, .allowBluetoothA2DP, .allowBluetooth])
+        try audioSession.setPreferredSampleRate(16_000)
+        try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+        let recordingFormat = AVAudioFormat(commonFormat: .pcmFormatInt16, sampleRate: 16_000, channels: 1, interleaved: true)
+        let input = audioEngine.inputNode
+        let downMixer = AVAudioMixerNode()
+        audioEngine.attach(downMixer)
+        audioEngine.connect(input, to: downMixer, format: nil)
+        downMixer.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, time) in
+            snips.appendBuffer(buffer)
+        }
+        audioEngine.prepare()
+        return audioEngine
+    }
+    
+    fileprivate func setupHandlers() {
+        snips.onIntentDetected = { [weak self] intent in
+            DispatchQueue.main.sync {
+                self?.title = "Intent detected"
+                self?.logView.text = String(format:
+                    "Query: %@\n" +
+                    "Intent: %@\n" +
+                    "Probability: %.3f\n" +
+                    "Slots:\n\t%@",
+                    intent.input,
+                    intent.intent?.intentName ?? "",
+                    intent.intent?.probability ?? 0,
+                    intent.slots.map { "\($0.slotName): \($0.value)" }.joined(separator: "\n\t")
+                )
+            }
+        }
+        
+        snips.onHotwordDetected = { [weak self] in
+            DispatchQueue.main.sync {
+                self?.title = "🔔"
+            }
+        }
+        
+        snips.onListeningStateChanged = { [weak self] listening in
+            DispatchQueue.main.sync {
+                self?.title = listening ? "Listening..." : "Say the wake word"
+            }
+        }
+        
+        snips.snipsWatchHandler = { message in
+            DispatchQueue.main.sync {
+                NSLog("Snips log: \(message)")
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# quickstart-swift
+
+[![MIT License](https://img.shields.io/github/license/snipsco-samples/quickstart-swift.svg)](https://github.com/snipsco-samples/quickstart-swift/blob/master/LICENSE)
+![Swift 4.1.x](https://img.shields.io/badge/Swift-4.1.x-orange.svg)
+![Platforms](https://img.shields.io/badge/platforms-iOS%20-lightgrey.svg)
+
+Sample code for the iOS Quick Start guide on the Snips Dev Center.
+
+## Contributing
+
+Please see the [Contribution Guidelines](https://github.com/snipsco-samples/quickstart-swift/blob/master/CONTRIBUTING.md).
+
+## Copyright
+
+This library is provided by [Snips](https://www.snips.ai) as Open Source software. See [LICENSE](https://github.com/snipsco-samples/quickstart-swift/blob/master/LICENSE) for more information.
